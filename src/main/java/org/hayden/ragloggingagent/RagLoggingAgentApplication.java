37637e69b6069bde9434b10293e39ffe2f276c15
Hayden Eubanks <72290782+haydenubanx@users.noreply.github.com>
2025-08-07 17:11:15 +0100

    Initial commit

---
+++ src/main/java/org/hayden/ragloggingagent/RagLoggingAgentApplication.java
@@ -0,0 +1,13 @@
+package org.hayden.ragloggingagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagLoggingAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagLoggingAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hayden/ragloggingagent/RagLoggingAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hayden.ragloggingagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagLoggingAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
